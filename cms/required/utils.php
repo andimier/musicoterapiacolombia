15085c029fed98c88d1e70c84aeb5300693b146a
Andrés Montaña <andres.montana@prodigious.com>
2019-06-30 01:49:48 -0500

    [CMS] - change tabs for spaces on utils file and remove unnecesary methods

--- cms/required/utils.php
+++ cms/required/utils.php
@@ -1,5 +1,5 @@
 <?php
-	class Utils {
+    class Utils {
         function redirect_to($location = NULL) {
             if ($location !=NULL) {
                 header("Location: {$location}");
@@ -35,209 +35,140 @@
             global $connection;
             global $pFunctions;
 
-			$q = $pFunctions->getPhpQuery(
-				"SELECT * FROM secciones ORDER BY id ASC",
-				$connection
-			);
+            $q = $pFunctions->getPhpQuery(
+                "SELECT * FROM secciones ORDER BY id ASC",
+                $connection
+            );
 
-			if ($q) return $q;
+            if ($q) return $q;
 
-			return NULL;
+            return NULL;
 
         }
 
-		function traer_seccion_por_id($seccion_id){
-			global $connection;
-			$query = "SELECT * FROM secciones WHERE id=" . $seccion_id ." LIMIT 1";
+        function traer_seccion_por_id($seccion_id){
+            global $connection;
+            $query = "SELECT * FROM secciones WHERE id=" . $seccion_id ." LIMIT 1";
 
-			$result_set = mysql_query($query, $connection);
-			confirm_query($result_set);
+            $result_set = mysql_query($query, $connection);
+            confirm_query($result_set);
 
-			if($seccion = mysql_fetch_array($result_set)){
-				return $seccion;
-			}else{
-				return NULL;
-			}
-		}
+            if($seccion = mysql_fetch_array($result_set)){
+                return $seccion;
+            }else{
+                return NULL;
+            }
+        }
 
-		//==================== PUBLICACIONES =========================================
+        function traer_publicacion_seleccionada(){
+            global $publicacion_seleccionada;
 
-		function todas_las_publicaciones(){
-			global $connection;
-			$query = "SELECT * FROM publicaciones ORDER BY fecha DESC";
+            if(isset($_GET['publicacion_id'])){
+                $sel_publicacion = $_GET['publicacion_id'];
+                $publicacion_seleccionada = traer_publicacion_por_id($sel_publicacion);
+            }
+        }
 
-			$grupo_publicaciones = mysql_query($query, $connection);
-			confirm_query($grupo_publicaciones);
-			return $grupo_publicaciones;
-		}
+        function todas_las_noticias(){
+            global $connection;
+            $query = "SELECT * FROM noticias ORDER BY fecha DESC";
+            $grupo_noticias = mysql_query($query, $connection);
 
-		function traer_publicacion_por_id($publicacion_id){
-			global $connection;
-			$query = "SELECT * FROM publicaciones WHERE id =" . $publicacion_id ." LIMIT 1";
+            confirm_query($grupo_noticias);
+            return $grupo_noticias;
+        }
 
-			$result_set = mysql_query($query, $connection);
-			confirm_query($result_set);
+        function traer_noticia_por_id($noticia_id){
+            global $connection;
+            $query = "SELECT * FROM noticias WHERE id =" . $noticia_id ." LIMIT 1";
 
-			if($contenido = mysql_fetch_array($result_set)){
-				return $contenido;
-			}else{
-				return NULL;
-			}
-		}
+            $result_set = mysql_query($query, $connection);
+            confirm_query($result_set);
 
-		function traer_publicacion_seleccionada(){
-			global $publicacion_seleccionada;
+            if($contenido = mysql_fetch_array($result_set)){
+                return $contenido;
+            }else{
+                return NULL;
+            }
+        }
 
-			if(isset($_GET['publicacion_id'])){
-				$sel_publicacion = $_GET['publicacion_id'];
-				$publicacion_seleccionada = traer_publicacion_por_id($sel_publicacion);
-			}
-		}
+        function traer_noticia_seleccionada(){
+            global $noticia_seleccionada;
 
-		//============  IMAGENES PUBLICACIONES ==================//
-
-
-		function traer_imagenes_publicacion_por_id($id){
-			global $connection;
-			$query = "SELECT * FROM imagenes_publicaciones WHERE publicacion_id = $id";
-
-			$grupo_imagenes = mysql_query($query, $connection);
-			confirm_query($grupo_imagenes);
-			return $grupo_imagenes;
-		}
-
-
-		//==================== NOTICIAS =========================================
-
-		function todas_las_noticias(){
-			global $connection;
-			$query = "SELECT * FROM noticias ORDER BY fecha DESC";
-			$grupo_noticias = mysql_query($query, $connection);
-
-			confirm_query($grupo_noticias);
-			return $grupo_noticias;
-		}
-
-		function traer_noticia_por_id($noticia_id){
-			global $connection;
-			$query = "SELECT * FROM noticias WHERE id =" . $noticia_id ." LIMIT 1";
-
-			$result_set = mysql_query($query, $connection);
-			confirm_query($result_set);
-
-			if($contenido = mysql_fetch_array($result_set)){
-				return $contenido;
-			}else{
-				return NULL;
-			}
-		}
-
-		function traer_noticia_seleccionada(){
-			global $noticia_seleccionada;
-
-			if(isset($_GET['noticia_id'])){
-				$sel_noticia = $_GET['noticia_id'];
-				$noticia_seleccionada = traer_noticia_por_id($sel_noticia);
-			}
-		}
-
-		//============  IMAGENES NOTICIAS ==================//
-
-
-		/*function traer_imagenes_publicacion_por_id($id){
-			global $connection;
-			$query = "SELECT * FROM imagenes_publicaciones WHERE publicacion_id = $id";
-
-			$grupo_imagenes = mysql_query($query, $connection);
-			confirm_query($grupo_imagenes);
-			return $grupo_imagenes;
-		}
-		*/
-
-		//====================  CONTENIDOS  =========================================//
-
-		function traer_contenido_por_id($contenido_id){
-			global $connection;
-			$query = "SELECT * FROM contenidos WHERE id =" . $contenido_id ." LIMIT 1";
-
-			$result_set = mysql_query($query, $connection);
-			confirm_query($result_set);
-
-			if($contenido = mysql_fetch_array($result_set)){
-				return $contenido;
-			}else{
-				return NULL;
-			}
-		}
+            if(isset($_GET['noticia_id'])){
+                $sel_noticia = $_GET['noticia_id'];
+                $noticia_seleccionada = traer_noticia_por_id($sel_noticia);
+            }
+        }
 
 
 
-		//===============================  SECCION Y CONTENIDO SELECCIONADO ==========================//
+        //===============================  SECCION Y CONTENIDO SELECCIONADO ==========================//
 
-		function encontrar_seccion_y_contenido_seleccionados(){
+        function encontrar_seccion_y_contenido_seleccionados(){
 
-			global $seccion_seleccionada;
-			global $contenido_seleccionado;
+            global $seccion_seleccionada;
+            global $contenido_seleccionado;
 
-			if(isset($_GET['seccion'])){
-				$sel_seccion = $_GET['seccion'];
-				$seccion_seleccionada = traer_seccion_por_id($sel_seccion);
+            if(isset($_GET['seccion'])){
+                $sel_seccion = $_GET['seccion'];
+                $seccion_seleccionada = traer_seccion_por_id($sel_seccion);
 
-				$sel_contenido = "";
-				$contenido_seleccionado = NULL;
+                $sel_contenido = "";
+                $contenido_seleccionado = NULL;
 
-			}elseif(isset($_GET['contenido_id'])){
-				$sel_contenido = $_GET['contenido_id'];
-				$contenido_seleccionado = traer_contenido_por_id($sel_contenido);
+            }elseif(isset($_GET['contenido_id'])){
+                $sel_contenido = $_GET['contenido_id'];
+                $contenido_seleccionado = traer_contenido_por_id($sel_contenido);
 
-				$sel_seccion = "";
-				$seccion_seleccionada = NULL;
+                $sel_seccion = "";
+                $seccion_seleccionada = NULL;
 
-			}else{
-				$sel_seccion = "";
-				$sel_contenido = "";
-				$seccion_seleccionada = NULL;
-				$contenido_seleccionado = NULL;
-			}
-		}
+            }else{
+                $sel_seccion = "";
+                $sel_contenido = "";
+                $seccion_seleccionada = NULL;
+                $contenido_seleccionado = NULL;
+            }
+        }
 
-		//============================= ARTICULOS ====================================
+        //============================= ARTICULOS ====================================
 
-		function traer_articulo_por_id($articulo_id){
-			global $connection;
-			$query = "SELECT * FROM articulos WHERE id=" . $articulo_id ." LIMIT 1";
+        function traer_articulo_por_id($articulo_id){
+            global $connection;
+            $query = "SELECT * FROM articulos WHERE id=" . $articulo_id ." LIMIT 1";
 
-			$result_set = mysql_query($query, $connection);
-			confirm_query($result_set);
+            $result_set = mysql_query($query, $connection);
+            confirm_query($result_set);
 
-			if($articulo = mysql_fetch_array($result_set)){
-				return $articulo;
-			}else{
-				return NULL;
-			}
-		}
+            if($articulo = mysql_fetch_array($result_set)){
+                return $articulo;
+            }else{
+                return NULL;
+            }
+        }
 
-		function traer_articulo_seleccionado(){
-			global $articulo_seleccionado;
+        function traer_articulo_seleccionado(){
+            global $articulo_seleccionado;
 
-			if(isset($_GET['articulo_id'])){
-				$sel_articulo = $_GET['articulo_id'];
-				$articulo_seleccionado = traer_articulo_por_id($sel_articulo);
-			}
-		}
+            if(isset($_GET['articulo_id'])){
+                $sel_articulo = $_GET['articulo_id'];
+                $articulo_seleccionado = traer_articulo_por_id($sel_articulo);
+            }
+        }
 
-		//============  IMAGENES ARTICULOS ==================//
+        //============  IMAGENES ARTICULOS ==================//
 
-		function traer_imagenes_articulo_por_id($id){
-			global $connection;
-			$query = "SELECT * FROM imagenes_articulos WHERE articulo_id = $id";
+        function traer_imagenes_articulo_por_id($id){
+            global $connection;
+            $query = "SELECT * FROM imagenes_articulos WHERE articulo_id = $id";
 
-			$grupo_imagenes_articulo = mysql_query($query, $connection);
-			confirm_query($grupo_imagenes_articulo);
-			return $grupo_imagenes_articulo;
-		}
-	}
+            $grupo_imagenes_articulo = mysql_query($query, $connection);
+            confirm_query($grupo_imagenes_articulo);
+            return $grupo_imagenes_articulo;
+        }
+    }
 
-	$utils = new Utils();
+    $utils = new Utils();
 
 ?>
