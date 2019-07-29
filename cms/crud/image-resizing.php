113eddc3bfbbd8561172900bd705e419528ddc53
Andrés Montaña <andres.montana@prodigious.com>
2019-07-28 19:05:45 -0500

    [CMS] - set properties for image resizing and making creation images methods reusable

--- cms/crud/image-resizing.php
+++ cms/crud/image-resizing.php
@@ -1,37 +1,39 @@
 <?php
     class ImageCrop {
-        private static function createResourceFromPath($imagePath) {
-            return imagecreatefromjpeg($imagePath);
+        public static $srcImagePath = '';
+        public static $imageProps = [];
+        public static $squaredSizes = [300, 400, 500];
+
+        private static function createResourceFromPath() {
+            return imagecreatefromjpeg(self::$imageProps['srcImagePath']);
         }
 
-        private static function createDestinationResource() {
-            $dst_image = imagecreatetruecolor(200, 200);  //  esta funcion crea un rec negro con los parametros dados
+        private static function createDestinationResource($props) {
+            $dst_image = imagecreatetruecolor($props['dst_width'], $props['dst_height']);  //  esta funcion crea un rec negro con los parametros dados
 
             imagealphablending($dst_image, false);
             imagesavealpha($dst_image, true);
-
             $trans_colour = imagecolorallocatealpha($dst_image, 0, 0, 0, 127);
-
             imagefill($dst_image, 0, 0, $trans_colour);
 
             return $dst_image;
         }
 
-        private static function createResampledImageCopy($srcImagePath) {
-            $src_image = static::createResourceFromPath($srcImagePath);
-            $dst_image = static::createDestinationResource();
+        private static function createResampledImageCopy($prop) {
+            $src_image = static::createResourceFromPath();
+            $dst_image = static::createDestinationResource($prop);
 
             $dst_x = 0;
             $dst_y = 0;
 
-            $src_x = 0;
-            $src_y = 0;
+            $src_x = $prop['src_x'];
+            $src_y = $prop['src_y'];;
 
-            $dst_width = 200;
-            $dst_height = 200;
+            $dst_width = $prop['dst_width'];
+            $dst_height = $prop['dst_height'];
 
-            $src_w = 200;
-            $src_h = 200;
+            $src_w = $prop['src_width'];
+            $src_h = $prop['src_height'];
 
             imagecopyresampled(
                 $dst_image,
@@ -50,21 +52,42 @@
         }
 
         private static function getSquaredImageProps() {
-            $props = [];
+            $width = self::$imageProps['width'];
+            $height = self::$imageProps['height'];
+            $sizes = self::$squaredSizes;
 
-            if ($width > $heigh) {
-                $props['src_x'] = ($width / 2) - ($height / 2);
-                $props['src_width'] = $height;
-                $props['src_height'] = $height;
+            $p = [];
+            $src_x = 0;
+            $src_y = 0;
+
+            if ($width > $height) {
+                $src_x = ($width / 2) - ($height / 2);
+                $src_width = $height;
+                $src_height = $height;
             }
 
-            if ($height > $width){
-                $props['src_y'] = ($height / 2) - ($width / 2);
-                $props['src_width'] = $width;
-                $props['src_height'] = $width;
+            if ($height > $width) {
+                $src_y = ($height / 2) - ($width / 2);
+                $src_width = $width;
+                $src_height = $width;
             }
 
-            return $props;
+            array_push($sizes, $src_width);
+
+            for ($i = 0; $i < count($sizes); $i++) {
+                array_push($p, [
+                    'src_x' => $src_x,
+                    'src_y' => $src_y,
+                    'src_width' => $src_width,
+                    'src_height' => $src_height,
+                    "dst_x" => 0,
+                    "dst_y" => 0,
+                    "dst_width" => $sizes[$i],
+                    "dst_height" => $sizes[$i]
+                ]);
+            }
+
+            return $p;
         }
 
         private static function getRectangularprops() {
@@ -73,35 +96,47 @@
             $h = 500 / $scale_ratio;
         }
 
-        public static function createBaseSquaredImage() {
-            $props = static::getSquaredImageProps();
+        private static function getDestinationUrl($prop) {
+            $e = explode('.', static::$imageProps['imageName']);
+            $ext = end($e);
+            $folder = self::$imageProps['folder'];
 
-            $new_img_resource = static::createResampledImageCopy($srcImagePath);
-            // return imagejpeg($new_img_resource, $destination, 100);
+            return "{$folder}{$e[0]}_{$prop['dst_width']}_x_{$prop['dst_height']}.{$ext}";
         }
 
         private static function createSquaredImagesSet() {
-            $sizes = [300, 400, 500];
+            $props = static::getSquaredImageProps();
 
-            for ($i = 0; $i < count($sizes); $i++) {
-                $props = [
-                    "src_x" => 0,
-                    "src_y" => 0,
-                    "src_width" => $sizes[$i],
-                    "src_height" => $sizes[$i]
-                ];
+            for ($i = 0; $i < count($props); $i++) {
+                $new_img_resource = static::createResampledImageCopy($props[$i]);
+                $destination = self::getDestinationUrl($props[$i]);
 
-                $new_img_resource = static::createResampledImageCopy($srcImagePath, $props);
-                // return imagejpeg($new_img_resource, $destination, 100);
+                imagejpeg($new_img_resource, $destination, 100);
             }
         }
 
-        public static function createNewImagesSet($srcImagePath) {
-            // set base image
-            // set isSquaredImage
-            $isSquaredImage;
+        private static function setImageProps($srcImagePath) {
+            $p = getimagesize($srcImagePath);
 
-            if ($isSquaredImage) {
+            $props['srcImagePath'] = $srcImagePath;
+            $props['folder'] = '../images/';
+            $props['width'] = $p[0];
+            $props['height'] = $p[1];
+            $props['isSquaredImage'] = ($p[0] == $p[1]);
+
+            $ext_arr = explode('/', $p['mime']);
+            $props['extension'] = end($ext_arr);
+
+            $e = explode('/', $srcImagePath);
+            $props['imageName'] = end($e);
+
+            self::$imageProps = $props;
+        }
+
+        public static function createNewImagesSet($srcImagePath) {
+            self::setImageProps($srcImagePath);
+
+            if (self::$imageProps{'isSquaredImage'}) {
                 $props = [
                     "src_x" => 0,
                     "src_y" => 0,
@@ -112,12 +147,7 @@
                 static::createSquaredImagesSet();
             } else {
                 // build squared image
-                $baseSquaredImage = static::createBaseSquaredImage();
-
-                if ($baseSquaredImage) {
-                    $destination = '../images/small/img3.jpg';
-                    static::createSquaredImagesSet();
-                }
+                $baseSquaredImage = static::createSquaredImagesSet();
             }
         }
     }
