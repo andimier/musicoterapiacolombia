0a599302ca4da36845225a4acbf062feb4faa1a4
Andrés Montaña <andres.montana@prodigious.com>
2019-10-14 19:45:38 -0500

    [CMS] - Modify gult task to only include main.scss in css. Restore login styles with error box mesasge enhancements

--- cms/login.php
+++ cms/login.php
@@ -1,69 +1,35 @@
-<?php
-	require_once("required/login.php");
-?>
+<?php require_once("required/login.php"); ?>
 
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-	<title>ANDIMIER : ENTRADA ADMIN.CONTENIDOS</title>
-	<link rel="Stylesheet" type="text/css" href="css/main.css" />
-</head>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<title>ANDIMIER : ENTRADA ADMIN.CONTENIDOS</title>
+		<link rel="Stylesheet" type="text/css" href="css/main.css" />
+	</head>
 
-<body>
-	<!-- <div id="cabezote"></div> -->
-	<div id="pagina">
+	<body>
+		<header></header>
+		<main class="login-container">
+			<section class="login">
+				<form method="post">
+					<label>Nombre de Usuario</label>
+					<input name="username" type="text" value="<?php echo htmlentities($username);?>" maxlength="50" size="50" height="30" />
 
-		<div class="contenido1">
+					<label>Tu Contraseña</label>
+					<input type="password" name="password" maxlength="50" size="50" height="30" value="<?php echo $password; ?>" />
 
-			<form method="post">
-				<table>
-					<tr><td>Nombre de Usuario</td></tr>
-					<tr>
-						<td>
-							<input
-								name="username"
-								type="text"
-								value="<?php echo htmlentities($username);?>"
-								maxlength="50"
-								size="50"
-								height="30" />
-						</td>
-					</tr>
+					<input type="submit" name="submit" class="button" value="Ingresar" />
 
-					<tr><td>Tu Contraseña</td></tr>
-					<tr>
-						<td>
-							<input type="password" name="password" maxlength="50" size="50" height="30" value="<?php echo $password; ?>" />
-							<br />
-							<br />
-						</td>
-					</tr>
-					<tr>
-						<td>
-							<input type="submit" name="submit" class="boton" value="Ingresar" />
-						</td>
-					</tr>
-
-					<tr><td>
-					<br />
-					<hr />
-					<div class="mensajelogin">
-						<?php echo $error_message; ?>
-					</div>
-
-					</td></tr>
-
-				</table>
-			</form>
-
-		</div>
-	</div>
-</body>
+					<?php if (!empty($error_message)): ?>
+						<div class="login-message">
+							<?php echo $error_message; ?>
+						</div>
+					<?php endif; ?>
+				</form>
+			</section>
+		</main>
+	</body>
 </html>
 
-<?php
-if(isset($connection)){
-	mysqli_close($connection);
-}
-?>
+<?php if(isset($connection)) mysqli_close($connection); ?>
--- gulpfile-concat.js
+++ gulpfile-concat.js
@@ -4,16 +4,40 @@ const concatCss = require('gulp-concat-css');
 const removeFiles = require('gulp-remove-files');
 const browserSync = require('browser-sync').create();
 
+// const concat = require('gulp-concat'); // for js files
+// const bundleFiles = require('gulp-bundle-files');
+// const useref = require('gulp-useref');
+// const bundles = require('./sample-options.json');
+
+// function bundle() {
+//     return bundleFiles(bundles);
+// }
+
 const baseRoute = './cms/';
 const cssSrcFiles = ['./cms/css/login.css', './cms/css/shared.css'];
 const sourceCSSFiles = './cms/scss/**/*.scss';
 const sourceMainCSS = './cms/scss/main.scss';
 
 function styles() {
-    return gulp.src(sourceMainCSS)
+    return gulp.src(sourceCSSFiles)
         .pipe(sass())
         .pipe(gulp.dest('./cms/css/'))
         .pipe(browserSync.stream());
+}
+
+// Bundle all scss files into one
+// here must refresh beowser
+function bundle() {
+    return gulp.src(cssSrcFiles)
+        .pipe(concatCss('main.css'))
+        .pipe(gulp.dest('./cms/css/'))
+        .pipe(browserSync.stream());
+}
+
+// delete extra files
+function deleteFiles() {
+    return gulp.src(cssSrcFiles)
+        .pipe(removeFiles());
 }
 
 function watch() {
@@ -21,7 +45,9 @@ function watch() {
         proxy: 'localhost/musicoterapia/cms'
     });
 
-    gulp.watch(sourceCSSFiles, styles);
+    // chain functions
+    gulp.watch(sourceCSSFiles, gulp.series(styles, bundle, deleteFiles));
+
     gulp.watch('./cms/*.php').on('change', browserSync.reload);
     gulp.watch('./cms/components/**/*.php').on('change', browserSync.reload);
 };
