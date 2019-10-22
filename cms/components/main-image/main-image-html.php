9d8f67857baa3fd738c1303c25774f255b719159
Andrés Montaña <andres.montana@prodigious.com>
2019-10-21 21:17:16 -0500

    [CMS] - add semantics to MainImage component

--- cms/components/main-image/main-image-html.php
+++ cms/components/main-image/main-image-html.php
@@ -2,17 +2,17 @@
 <?php require_once('main-image.php'); ?>
 
 <div class="main-image" data-component="main-image">
-    <img src="<?php echo $mainImageDataArr['image']; ?>" width="165"/>
+    <section class="new-file-wrapper">
+        <img src="<?php echo $mainImageDataArr['image']; ?>" width="165"/>
     <!-- <a href="crud/update-image.php?contentType=section">a crud por favor</a> -->
 
-    <section class="new-file-wrapper">
         <form enctype="multipart/form-data" method="post" action="crud/update-image.php">
             <input type="hidden" name="contentId" value="<?php echo $mainImageDataArr['contentId']; ?>" />
             <input type="hidden" name="contentType" value="<?php echo $mainImageDataArr['contentType']; ?>" />
             <input type="hidden" name="image" value="<?php echo $mainImageDataArr['image']; ?>" />
             <input type="hidden" name="currentUrl" value="<?php echo Utils::getCurrentUrl(); ?>" />
 
-            <p>Selecciona una imagen de tu computador</p>
+            <h3>Selecciona una imagen de tu computador</h3>
             <label for="image-file" class="button select-image-btn">Seleccionar imagen</label>
             <input id="image-file"
                 type="file"
