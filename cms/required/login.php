e07c33cb17b962d8485c6f304e4b7718bbabb122
Andrés Montaña <andres.montana@prodigious.com>
2019-10-22 20:13:36 -0500

    [CMS] - chage error messages to match the ones from the login input labels

--- cms/required/login.php
+++ cms/required/login.php
@@ -13,8 +13,8 @@
 
         foreach($required_fields as $fieldname){
             if (!isset($_POST[$fieldname]) || (empty($_POST[$fieldname])  && !is_numeric($_POST[$fieldname]))) {
-                if ($fieldname == 'username') $errors[] = 'usuario';
-                if ($fieldname == 'password') $errors[] = 'contraseña';
+                if ($fieldname == 'username') $errors[] = 'Nombre Usuario';
+                if ($fieldname == 'password') $errors[] = 'Tu Contraseña';
             }
         }
 
