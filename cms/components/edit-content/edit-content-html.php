8c13053502d7fcf18b654bc067717f1dccaf3531
Andrés Montaña <andres.montana@prodigious.com>
2020-01-07 18:27:05 -0500

    [CMS] - add content editing textbox paste text tags stripping functionality

--- cms/components/edit-content/edit-content-html.php
+++ cms/components/edit-content/edit-content-html.php
@@ -20,4 +20,75 @@
             <input type="submit" name="insertar_contenido" id="insertar_contenido" class="fondo_azul" value="insertar contenido"/>
         </form>
     </section> -->
+
+    <?php require_once('components/text-edit-form/text-edit-form-html.php'); ?>
+    <script>
+
+        var textLength = document.querySelector('#caja2').innerText.length;
+
+        function parseLink(link, text) {
+            var hRef = link.match(/href=[-_\/\w":.;?&=]+/);
+
+            return "<a " + hRef + ">" + text + "</a>";
+        }
+
+        function getLinksText(link) {
+            var div = document.createElement('DIV');
+            div.innerHTML = link;
+            var text = div.innerText;
+
+            return text;
+        }
+
+        function getLinks(html, links) {
+            var str = html;
+            var linksLength = html.match(/<a/g) && html.match(/<a/g).length;
+
+            if (linksLength) {
+                for (var i = 0; i < linksLength; i++) {
+                    var startIndex = str.indexOf('<a');
+                    var endIndex = str.indexOf('</a>') + 4;
+                    var link = str.slice(startIndex, endIndex);
+                    var str = str.slice(endIndex);
+
+                    var text = getLinksText(link);
+                    var parsedLink = parseLink(link, text);
+
+                    if (text) {
+                        links.push({'link': parsedLink, 'text': text});
+                    }
+                }
+
+                return links;
+            }
+        }
+
+        function replaceText(textbox, links) {
+            var text = textbox;
+
+            for (var i = 0; i < links.length; i++) {
+                text = text.replace(links[i].text, links[i].link);
+            }
+
+            return text;
+        }
+
+        document.querySelector('#caja2').addEventListener('input', function() {
+            var text = this.innerText;
+
+            if ((text.length - textLength) > 1) {
+                var html = this.innerHTML;
+                var links = getLinks(html, []);
+
+                if (links) {
+                    var newText = replaceText(this.innerText, links);
+                    this.innerHTML = newText;
+
+                    document.querySelector('textarea').value = newText;
+                }
+            }
+
+            textLength = this.innerText.length;
+        });
+    </script>
 </div>
