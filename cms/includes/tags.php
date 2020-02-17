0058b675712a31d790993738acc168685ba31887
andimier <andimier@gmail.com>
2020-02-16 22:33:42 -0500

    CMS - change encoding

--- cms/includes/tags.php
+++ cms/includes/tags.php
@@ -1,4 +1,4 @@
-<meta  http-equiv="Content-Type" content="text/html; charset=iso-UTF-8" />
+<meta  http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 <title>Musicoterapia Colombia</title>
 <meta name="robots" content="noindex, nofollow" />
 <meta name="googlebot" content="noindex" />
