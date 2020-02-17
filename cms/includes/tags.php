d8c13175d997173a2a9f45810fb3ab21b43776ab
andimier <andimier@gmail.com>
2020-02-16 22:28:42 -0500

    CMS - change cms encoding to utf8

--- cms/includes/tags.php
+++ cms/includes/tags.php
@@ -1,4 +1,4 @@
-<meta  http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<meta  http-equiv="Content-Type" content="text/html; charset=iso-UTF-8" />
 <title>Musicoterapia Colombia</title>
 <meta name="robots" content="noindex, nofollow" />
 <meta name="googlebot" content="noindex" />
