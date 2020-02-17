de6e8be8d24488a93df9fd47ce3e1ac76e8732b4
andimier <andimier@gmail.com>
2020-02-16 21:38:15 -0500

    CMS - syntaxis update

--- cms/required/connection.php
+++ cms/required/connection.php
@@ -5,13 +5,12 @@
 	mysqli_set_charset($connection, "utf8");
 
 	if (!$connection) {
-		die("Datos Incorrectos: " . mysql_error($connection));
+		die("Datos Incorrectos: " . mysqli_error($connection));
 	}
 
 	$db_select = mysqli_select_db($connection, "musicote_base");
 
 	if (!$db_select) {
-		die("La Seleccion de la base de datos fallo: " . mysql_error($connection));
-
+		die("La Seleccion de la base de datos fallo: " . mysqli_error($connection));
 	}
 ?>
